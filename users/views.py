a7da3339e7787c27ab2ef03066049c778b4d2d49
Ahsanuzzman Khan <ahsan.khan434@gmail.com>
2013-11-14 20:17:46 +0600

    Modified...

--- users/views.py
+++ users/views.py
@@ -4,12 +4,14 @@ from users.models import *
 from users.forms import *
 
 def register(request):
+    #pas = hashers.make_password('ahsan')
+    #import pdb; pdb.set_trace()
     if request.method == 'POST':
         form = RegisterForm(request.POST, request.FILES)
         if form.is_valid():
             #Process data in form.clean_data and save to database
             recipients = form.cleaned_data.get('email')
-            form.clean_data['password'] = hashers.make_password(form.cleaned_data.get('password'))
+            form.clean_data['password'] = hashers.make_password(form.cleaned_data.get('password'), None, 'md5')
             user = Users(form.cleaned_data)
             from django.core.mail import send_mail
             send_mail('subject', 'message', 'info@zapscms.com', recipients)
