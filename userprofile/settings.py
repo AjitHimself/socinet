f1df2d46e1266e90bf34a622da99cfa0a2dc1c8c
Ahsanuzzman Khan <ahsan.khan434@gmail.com>
2013-11-18 22:58:11 +0600

    Authentication functionality added

--- userprofile/settings.py
+++ userprofile/settings.py
@@ -92,7 +92,6 @@ TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
 #     'django.template.loaders.eggs.Loader',
-    os.path.join(PROJECT_PATH,'templates'),
 )
 
 MIDDLEWARE_CLASSES = (
@@ -114,6 +113,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_PATH,'templates'),
 )
 
 INSTALLED_APPS = (
--- users/views.py
+++ users/views.py
@@ -1,25 +1,51 @@
 from django.contrib.auth import hashers
 from django.shortcuts import render, redirect
-from users.models import *
+from users.models import Users
 from users.forms import *
 
 def register(request):
-    #pas = hashers.make_password('ahsan')
-    #import pdb; pdb.set_trace()
+
     if request.method == 'POST':
         form = RegisterForm(request.POST, request.FILES)
         if form.is_valid():
             #Process data in form.clean_data and save to database
             recipients = form.cleaned_data.get('email')
-            form.clean_data['password'] = hashers.make_password(form.cleaned_data.get('password'), None, 'md5')
+            form.clean_data['password'] = hashers.make_password(form.cleaned_data.get('password'))
             user = Users(form.cleaned_data)
-            from django.core.mail import send_mail
-            send_mail('subject', 'message', 'info@zapscms.com', recipients)
+            #from django.core.mail import send_mail
+            #send_mail('subject', 'message', 'info@zapscms.com', recipients)
             user.save()
             return redirect('/')
     else:
         form = RegisterForm()
 
-    return render(request, 'register.html', {
+    return render(request, 'users/register.html', {
         'form': form
     })
+
+def login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            try:
+                u = Users.objects.get(email=request.POST['email'])
+                if u.password == hashers.make_password(request.POST['password']):
+                    request.session['user'] = u
+                    return redirect('/')
+            except Users.DoesNotExist:
+                pass
+
+    else:
+        form = LoginForm()
+
+    return render(request, 'users/login.html', {
+        'form': form
+    })
+
+def logout(request):
+    try:
+        del request.session['user']
+    except KeyError:
+        pass
+
+    return redirect('/')
